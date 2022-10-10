d6de2c133f1bb60a675d0588c7df06a4165ceaf6
Jose Manuel Bilbao <jose@mymoodbit.com>
2022-10-10 18:25:06 -0400

    add poke option

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="pokemon-container">
-    <img class="hidden-pokemon" :src="getUrl()" alt="">
+    <img v-if="!show" class="hidden-pokemon" :src="getUrl()" alt="">
+    <img v-if="show" class="fade-in" :src="getUrl()" alt="">
   </div>
 </template>
 
 <script>
 export default {
   name: "PokemonPicture",
+  props: {
+    idPokemon: Number,
+    show: {
+      type: Boolean,
+      required: true
+    }
+  },
   methods: {
     getUrl() {
-      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/3.svg`;
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.idPokemon}.svg`;
     }
   }
 }
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,14 +1,52 @@
 <template>
-  <PokemonPicture/>
+  <div v-if="pokemon !== null">
+    <h1>¿QUIEN ES ESTE POKEMON?</h1>
+    <PokemonPicture :show="showPokemon" :id-pokemon="pokemon.id"/>
+    <PokemonOption v-if="!showPokemon" :lista="pokemons" @pokeSelect="selectAnswer($event)"/>
+    <h3>{{ message }}</h3>
+  </div>
+  <p v-if="pokemon === null">Loading...</p>
 </template>
 
 <script>
 import PokemonPicture from "@/components/PokemonPicture";
+import getList from "@/helpers/pokemonlist";
+import PokemonOption from "@/components/PokemonOption";
 
 export default {
   name: "PokemonPage",
   components: {
-    PokemonPicture
+    PokemonPicture,
+    PokemonOption
+  },
+  data() {
+    return {
+      pokemons: [],
+      pokemon: null,
+      showPokemon: false,
+      message: null
+    }
+  },
+  methods: {
+    async loadPokemons() {
+      this.pokemons = await getList();
+      console.log(this.pokemons);
+      const pos = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemons[pos];
+    },
+    selectAnswer(id){
+      this.showPokemon = true;
+      if (id === this.pokemon.id) {
+          console.log('correcto');
+          this.message = 'Correcto! es ' + this.pokemon.name;
+      } else {
+          console.log('incorrecto');
+        this.message = 'InCorrecto! es ' + this.pokemon.name + ' :(';
+      }
+    }
+  },
+  mounted() {
+    this.loadPokemons();
   }
 }
 </script>
